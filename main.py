5340bceb7db7ad11ead73b75c97e619fc0b87275
Mitchgut278 <gutmanmitchell@gmail.com>
2023-10-11 19:48:47 -0400

    add score

--- main.py
+++ main.py
@@ -116,8 +116,12 @@ class MAIN:
         self.check_fail()
     
     def draw_elements(self):
+        
+        self.draw_grass()
         self.fruit.draw_fruit()
         self.snake.draw_snake()
+        self.draw_score()
+        
 
     def check_collision(self):
         if self.fruit.pos == self.snake.body[0]:
@@ -131,9 +135,38 @@ class MAIN:
         # check if snake hits itself
         if self.snake.body[0] in self.snake.body[1:]:
             self.game_over()
+
     def game_over(self):
         pygame.quit()
         sys.exit()
+    
+    def draw_grass(self):
+        grass_color = (167, 209 , 61)
+        for row in range(cell_number):
+            if row % 2 == 0:
+                for col in range(cell_number):
+                    if col % 2 == 0:
+                        grass_rect = pygame.Rect(col * cell_size, row * cell_size, cell_size, cell_size)
+                        pygame.draw.rect(screen, grass_color, grass_rect)
+            else:
+                for col in range(cell_number):
+                    if col % 2 != 0:
+                        grass_rect = pygame.Rect(col * cell_size, row * cell_size, cell_size, cell_size)
+                        pygame.draw.rect(screen, grass_color, grass_rect)
+    
+    def draw_score(self):
+        score_text = str(len(self.snake.body) - 3)
+        score_surf = game_font.render(score_text, True, (56,74,12))
+        score_x = int(cell_size * cell_number - 60)
+        score_y = int(cell_size * cell_number - 40)
+        score_rect = score_surf.get_rect(center = (score_x, score_y))
+        apple_rect = apple.get_rect(midright = (score_rect.left, score_rect.centery))
+        bg_rect = pygame.Rect(apple_rect.left, apple_rect.top, apple_rect.width + score_rect.width + 6, apple_rect.height)
+        pygame.draw.rect(screen, (165,209,61), bg_rect)
+        pygame.draw.rect(screen, (56,74,12), bg_rect, 2)
+        screen.blit(score_surf, score_rect)
+        screen.blit(apple, apple_rect)
+
 
 pygame.init()
 cell_size = 40
@@ -143,7 +176,7 @@ screen = pygame.display.set_mode((cell_number * cell_size, cell_number * cell_si
 # used to set fps
 clock = pygame.time.Clock()
 apple = pygame.image.load('Graphics/apple.png').convert_alpha()
-
+game_font = pygame.font.Font('Font/PoetsenOne-Regular.ttf', 25)
 main_game = MAIN()
 
 SCREEN_UPDATE = pygame.USEREVENT
